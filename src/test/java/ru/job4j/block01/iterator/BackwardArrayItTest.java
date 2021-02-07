506b6604eff4028fccbe8586103fe24fe71ddcff
konstantin <kotmix@bk.ru>
2021-02-07 21:36:16 +0400

    1. Что такое итератор. [#454486]

---
+++ src/test/java/ru/job4j/block01/iterator/BackwardArrayItTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.block01.iterator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+public class BackwardArrayItTest {
+    @Test
+    public void whenMultiCallhasNextThenTrue() {
+        BackwardArrayIt it = new BackwardArrayIt(new int[] {1, 2, 3});
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+    }
+
+    @Test
+    public void whenReadSequence() {
+        BackwardArrayIt it = new BackwardArrayIt(new int[] {1, 2, 3});
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(1));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenNextFromException() {
+        BackwardArrayIt it = new BackwardArrayIt(new int[] {});
+        it.next();
+    }
+}
