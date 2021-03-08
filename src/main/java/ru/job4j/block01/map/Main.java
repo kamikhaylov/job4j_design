55552c9d129a2081e86f3b79f5d38611926eacd4
konstantin <kotmix@bk.ru>
2021-03-08 08:41:33 +0400

    5. Переопределить и equals и hashCode [#454506]
    Signed-off-by: konstantin <kotmix@bk.ru>

--- src/main/java/ru/job4j/block01/map/Main.java
+++ src/main/java/ru/job4j/block01/map/Main.java
@@ -8,8 +8,6 @@ public class Main {
     public static void main(String[] args) {
         User user1 = new User("Name", 1, new GregorianCalendar(1990, 1, 1));
         User user2 = new User("Name", 1, new GregorianCalendar(1990, 1, 1));
-        System.out.println(user1.hashCode());
-        System.out.println(user2.hashCode());
         Map<User, Object> map = new HashMap<>();
         map.put(user1, new Object());
         map.put(user2, new Object());
