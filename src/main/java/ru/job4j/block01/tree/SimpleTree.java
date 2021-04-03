92df280a9df80a680365694657763ceb0bbd9379
konstantin <kotmix@bk.ru>
2021-04-04 00:01:10 +0400

    2. Добавить метод boolean isBinary() [#454495]
    Signed-off-by: konstantin <kotmix@bk.ru>

--- src/main/java/ru/job4j/block01/tree/SimpleTree.java
+++ src/main/java/ru/job4j/block01/tree/SimpleTree.java
@@ -1,6 +1,7 @@
 package ru.job4j.block01.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -23,12 +24,22 @@ class SimpleTree<E> implements Tree<E> {
 
     @Override
     public Optional<Node<E>> findBy(E value) {
+        return findByPredicate(p -> p.getValue().equals(value));
+    }
+
+    @Override
+    public boolean isBinary() {
+        Optional<Node<E>> rsl = findByPredicate(p -> p.getChildren().size() > 2);
+        return rsl.isPresent() ? false : true;
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.getValue().equals(value)) {
+            if (condition.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
--- src/main/java/ru/job4j/block01/tree/Tree.java
+++ src/main/java/ru/job4j/block01/tree/Tree.java
@@ -25,4 +25,6 @@ public interface Tree<E> {
             return children;
         }
     }
+
+    boolean isBinary();
 }
