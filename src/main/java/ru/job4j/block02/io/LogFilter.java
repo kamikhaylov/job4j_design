9856f711d7d3b8e8eb03d045cd8fab138261295c
konstantin <kotmix@bk.ru>
2021-04-20 23:40:09 +0400

    0.3. BufferedReader. [#454526]
    Signed-off-by: konstantin <kotmix@bk.ru>

---
+++ src/main/java/ru/job4j/block02/io/LogFilter.java
@@ -0,0 +1,27 @@
+package ru.job4j.block02.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+        List<String> rsl = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            for (String line = in.readLine();  line != null; line = in.readLine()) {
+                if (line.contains("404")) {
+                    rsl.add(line + System.lineSeparator());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("data//log.txt");
+        System.out.println(log);
+    }
+}
