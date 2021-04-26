5e8c2de55361cb4159c0f2ed6eb071b85f557595
konstantin <kotmix@bk.ru>
2021-04-26 11:38:20 +0400

    2. Анализ доступности сервера. [#454532] Fix
    Signed-off-by: konstantin <kotmix@bk.ru>

--- src/main/java/ru/job4j/block02/io/Analizy.java
+++ src/main/java/ru/job4j/block02/io/Analizy.java
@@ -5,22 +5,19 @@ import java.io.*;
 public class Analizy {
     public void unavailable(String source, String target) {
         boolean startPeriod = false;
-        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-            try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-                for (String line = read.readLine();  line != null; line = read.readLine()) {
-                    String[] arr = line.split(" ");
-                    if ((arr[0].equals("400") || arr[0].equals("500"))
-                            && !startPeriod) {
-                        startPeriod = true;
-                        out.print(arr[1] + ";");
-                    } else if (!arr[0].equals("400") && !arr[0].equals("500")
-                            && startPeriod) {
-                        startPeriod = false;
-                        out.println(arr[1]);
-                    }
+        try (BufferedReader read = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            for (String line = read.readLine();  line != null; line = read.readLine()) {
+                String[] arr = line.split(" ");
+                if ((arr[0].equals("400") || arr[0].equals("500"))
+                        && !startPeriod) {
+                    startPeriod = true;
+                    out.print(arr[1] + ";");
+                } else if (!arr[0].equals("400") && !arr[0].equals("500")
+                        && startPeriod) {
+                    startPeriod = false;
+                    out.println(arr[1]);
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         } catch (IOException e) {
             e.printStackTrace();
