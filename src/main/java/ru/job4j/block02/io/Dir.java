0bcd6a326a8165fbbbb28656dfcf73b9d08c6b03
konstantin <kotmix@bk.ru>
2021-04-27 22:04:44 +0400

    5. Валидация параметров запуска. [#454524]
    Signed-off-by: konstantin <kotmix@bk.ru>

--- src/main/java/ru/job4j/block02/io/Dir.java
+++ src/main/java/ru/job4j/block02/io/Dir.java
@@ -5,7 +5,10 @@ import java.util.Objects;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("/Users/konstantin/Desktop/MyMac/JAVAWORK/job4j/projects");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(
                     String.format("Not exist %s", file.getAbsoluteFile())
@@ -18,10 +21,12 @@ public class Dir {
         }
         System.out.printf("size : %s%n", file.getTotalSpace());
         for (File subfile : Objects.requireNonNull(file.listFiles())) {
-            System.out.println(subfile.getName()
-                    + " - "
-                    + subfile.getAbsoluteFile().length()
-            );
+            if (subfile.getName().contains(args[1])) {
+                System.out.println(subfile.getName()
+                        + " - "
+                        + subfile.getAbsoluteFile().length()
+                );
+            }
         }
     }
 }
