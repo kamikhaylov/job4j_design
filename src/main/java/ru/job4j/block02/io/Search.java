eb8f2a596c0a92585833ddeeeaccdb3f944f142c
konstantin <kotmix@bk.ru>
2021-04-29 20:19:09 +0400

    5. Валидация параметров запуска. [#454524] Fix
    Signed-off-by: konstantin <kotmix@bk.ru>

--- src/main/java/ru/job4j/block02/io/Search.java
+++ src/main/java/ru/job4j/block02/io/Search.java
@@ -9,9 +9,9 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
+        if (args.length != 2) {
             throw new IllegalArgumentException(
-                    "Root folder is null. Usage java -jar search.jar ROOT_FOLDER."
+                    "Root folder is null. Usage java -jar search.jar ROOT_FOLDER, FILE_EXTENSION."
             );
         }
         Path start = Paths.get(args[0]);
