69fe6e2cc60bfb3d946d31ceb60bfa9646eedd21
konstantin <kotmix@bk.ru>
2021-05-03 00:25:18 +0400

    6. Кодировка. [#454534]
    Signed-off-by: konstantin <kotmix@bk.ru>

---
+++ src/main/java/ru/job4j/block02/io/chat/ConsoleChat.java
@@ -0,0 +1,82 @@
+package ru.job4j.block02.io.chat;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsoleChat {
+    private final String path;
+    private final String botAnswers;
+    private final List<String> messagesBot;
+    private boolean enableBot;
+    private final Charset charset;
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+        this.messagesBot = new ArrayList<>();
+        this.enableBot = true;
+        this.charset = StandardCharsets.UTF_8;
+    }
+
+    public void run() {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
+            try (BufferedWriter out = new BufferedWriter(
+                    new FileWriter(path, charset, true))) {
+                getMessagesListBot();
+                String messageUser = in.readLine();
+                out.write(messageUser + System.lineSeparator());
+                while (checkOut(messageUser)) {
+                    checkEnableBot(messageUser);
+                    if (enableBot) {
+                        String messageBot = getRandomMessageBot();
+                        System.out.println(messageBot);
+                        out.write(messageBot + System.lineSeparator());
+                    }
+                    messageUser = in.readLine();
+                    out.write(messageUser + System.lineSeparator());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean checkOut(String message) {
+        return !message.equals(OUT);
+    }
+
+    private void checkEnableBot(String message) {
+        if (message.equals(STOP)) {
+            enableBot = false;
+        } else if (message.equals(CONTINUE)) {
+            enableBot = true;
+        }
+    }
+
+    private void getMessagesListBot() {
+        try (BufferedReader in = new BufferedReader(new FileReader(botAnswers))) {
+            for (String line = in.readLine();  line != null; line = in.readLine()) {
+                messagesBot.add(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getRandomMessageBot() {
+        return messagesBot.get((int) (Math.random() * (messagesBot.size() - 1)));
+    }
+
+    public static void main(String[] args) {
+        String path = "./data/chat/log.txt";
+        String botAnswers = "./data/chat/answers.txt";
+        ConsoleChat cc = new ConsoleChat(path, botAnswers);
+        cc.run();
+    }
+}
