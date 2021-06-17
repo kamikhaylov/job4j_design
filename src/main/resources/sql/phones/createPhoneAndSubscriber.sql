acc01b2dc537c2cde5399924d49b8d0e74229a5c
konstantin <kotmix@bk.ru>
2021-06-17 22:07:50 +0400

    0.1. Связи между таблицами [#373615] Fix
    Signed-off-by: konstantin <kotmix@bk.ru>

--- src/main/resources/sql/phones/createPhoneAndSubscriber.sql
+++ src/main/resources/sql/phones/createPhoneAndSubscriber.sql
@@ -1,16 +1,12 @@
 create table phone(
 	id serial primary key,
 	prefix varchar(5),
-	number int
+	number int,
+	subscriber_id int references subscriber(id) unique
 );
 
 create table subscriber(
 	id serial primary key,
-	name varchar(255)
-);
-
-create table phone_subscriber(
-	id serial primary key,
-	phone_id int references phone(id) unique,
-	subscriber_id int references subscriber(id) unique
+	name varchar(255),
+	phone_id int references phone(id) unique
 );
