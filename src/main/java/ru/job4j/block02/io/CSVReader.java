c73a9ad28e5db5e8720cc5faf2eb9bf51960fd82
konstantin <kotmix@bk.ru>
2021-07-27 21:40:45 +0400

    7. Scanner [#504791 #179875] Fix
    Signed-off-by: konstantin <kotmix@bk.ru>

--- src/main/java/ru/job4j/block02/io/CSVReader.java
+++ src/main/java/ru/job4j/block02/io/CSVReader.java
@@ -2,15 +2,15 @@ package ru.job4j.block02.io;
 
 import java.io.*;
 import java.nio.file.Path;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class CSVReader {
-    private Path path;
-    private String delimiter;
-    private String out;
-    private String filter;
-    private ArrayList<String> argsFilter;
+    private final Path path;
+    private final String delimiter;
+    private final String out;
+    private final String filter;
 
     public CSVReader(ArgsName argsName) {
         this.path = Path.of(argsName.get("path"));
@@ -19,67 +19,56 @@ public class CSVReader {
         this.filter = argsName.get("filter");
     }
 
-    private void getArgsFilter(String out) {
-        argsFilter = new ArrayList<>();
-        Scanner scanner = new Scanner(new ByteArrayInputStream(out.getBytes()))
-                .useDelimiter(",");
-        int i = 0;
-        while (scanner.hasNext()) {
-            argsFilter.add(scanner.next());
-        }
-    }
-
-    public void reader() {
-        getArgsFilter(filter);
+    public void reader() throws IOException {
+        StringBuilder outPrint = resultOut(getArgsFilter(filter));
         if (out.equals("stdout")) {
-            stdOut();
-        } else if (out.equals("path")) {
-            fileOut();
-        }
-    }
-
-    private void stdOut() {
-        try (Scanner scArgs = new Scanner(path).useDelimiter(delimiter)) {
-            while (scArgs.hasNext()) {
-                String[] arr = scArgs.next().split("=");
-                if (argsFilter.contains(arr[0])) {
-                    System.out.println(arr[1]);
-                }
+            System.out.println(outPrint);
+        } else {
+            try (FileWriter writer = new FileWriter(out)) {
+                writer.write(String.valueOf(outPrint));
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
-    private void fileOut() {
-        try (Scanner scArgs = new Scanner(path).useDelimiter(delimiter)) {
-            while (scArgs.hasNext()) {
-                String[] arr = scArgs.next().split("=");
-                if (argsFilter.contains(arr[0])) {
-                    try (PrintWriter out = new PrintWriter(
-                            new BufferedOutputStream(
-                                    new FileOutputStream("data/csv/resultFile.txt", true)
-                            ))) {
-                        out.write(arr[1] + System.lineSeparator());
-                    } catch (Exception e) {
-                        e.printStackTrace();
+    private int[] getArgsFilter(String filter) throws IOException {
+        Scanner scanner = new Scanner(path).useDelimiter(System.lineSeparator());
+        String[] columns = scanner.next().split(delimiter);
+        return IntStream.range(0, columns.length)
+                .filter(i -> filter.contains(columns[i])).toArray();
+    }
+
+    private StringBuilder resultOut(int[] indexFilter) {
+        StringBuilder result = new StringBuilder();
+        try (Scanner scArgs = new Scanner(path).useDelimiter(System.lineSeparator())) {
+            while (scArgs.hasNextLine()) {
+                List<String> outColumns = List.of(scArgs.next().split(delimiter));
+                for (int i = 0; i < indexFilter.length; i++) {
+                    result.append(outColumns.get(indexFilter[i]));
+                    if (i != indexFilter.length - 1) {
+                        result.append(";");
                     }
                 }
+                result.append(System.lineSeparator());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return result;
     }
 
-    public static void main(String[] args) {
+    public static void checkArgs(String[] args) {
         if (args.length != 4) {
             throw new IllegalArgumentException(
                     "Root folder is null. Usage java -jar search.jar FILE_PATH,"
                             + "DELIMITER, "
-                            + "OUTPUT (key = stdout or path), "
+                            + "OUTPUT (key = stdout or data/csv/resultFile.txt, "
                             + "FILTER (name,age,birthDate,education,children)"
             );
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        checkArgs(args);
         CSVReader csvReader = new CSVReader(ArgsName.of(args));
         csvReader.reader();
     }
