cc98540694e559d48b4a5743a079b741895ea0cf
konstantin <kotmix@bk.ru>
2022-01-12 21:46:04 +0400

    2. Парковка машин [#853 #249191]
    Signed-off-by: konstantin <kotmix@bk.ru>

--- src/main/java/ru/job4j/block04/lsp/parking/ParkingCars.java
+++ src/main/java/ru/job4j/block04/lsp/parking/ParkingCars.java
@@ -7,6 +7,8 @@ public class ParkingCars implements Parking {
     private int placesCars;
     private int placesTracks;
     private List<Car> cars;
+    private int occupiedPlacesCars;
+    private int occupiedPlacesTracks;
 
     public ParkingCars(int placesCars, int placesTracks) {
         this.placesCars = placesCars;
@@ -16,10 +18,63 @@ public class ParkingCars implements Parking {
 
     @Override
     public boolean add(Car car) {
-        return false;
+        boolean rsl = false;
+        if (car.getSize() == 1) {
+            rsl = addCar(car);
+        } else if (car.getSize() > 1) {
+            rsl = addTrack(car);
+        }
+        return rsl;
+    }
+
+    private boolean addCar(Car car) {
+        boolean rsl = false;
+        if (car.getSize() <= placesCars && occupiedPlacesCars < placesCars) {
+            cars.add(car);
+            occupiedPlacesCars++;
+            rsl = true;
+        } else {
+            throw new IllegalArgumentException(
+                    "Нет свободных парковочных мест для легкового автомобиля");
+        }
+        return rsl;
+    }
+
+    private boolean addTrack(Car car) {
+        boolean rsl = false;
+        if (occupiedPlacesTracks < placesTracks) {
+            rsl = addTrackInParkingTrack(car);
+        } else if (car.getSize() <= placesCars && occupiedPlacesCars < placesCars) {
+            rsl = addTrackInParkingCar(car);
+        } else {
+            throw new IllegalArgumentException(
+                    "Нет свободных парковочных мест для грузового автомобиля"
+                           + "на местах грузовых и легковых автомобилей");
+        }
+        return rsl;
+    }
+
+    private boolean addTrackInParkingTrack(Car car) {
+        cars.add(car);
+        occupiedPlacesTracks++;
+        return true;
+    }
+
+    private boolean addTrackInParkingCar(Car car) {
+        for (int i = 0; i < car.getSize(); i++) {
+            if (occupiedPlacesCars < placesCars) {
+                cars.add(car);
+                occupiedPlacesCars++;
+            } else {
+                throw new IllegalArgumentException(
+                        "Нет свободных парковочных мест для грузового автомобиля"
+                               + "на местах леговых автомобилей");
+            }
+        }
+        return true;
     }
 
     public List<Car> findAll() {
-        return new ArrayList<>();
+        return (List<Car>) ((ArrayList<Car>) this.cars).clone();
     }
 }
--- src/test/java/ru/job4j/block04/lsp/parking/ParkingCarsTest.java
+++ src/test/java/ru/job4j/block04/lsp/parking/ParkingCarsTest.java
@@ -1,52 +1,37 @@
 package ru.job4j.block04.lsp.parking;
 
-import org.junit.Ignore;
 import org.junit.Test;
-import java.util.ArrayList;
 import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class ParkingCarsTest {
-    @Ignore
     @Test
     public void thenOneCarWhenOneParkingCars() {
-        ParkingCars parking = new ParkingCars(2, 2);
+        ParkingCars parking = new ParkingCars(1, 0);
         Car car = new Car("Car1", 1);
         parking.add(car);
-        List<Car> expected = new ArrayList<>(4);
-        expected.add(car);
-        assertThat(parking.findAll(), is(expected));
+        assertThat(parking.findAll(), is(List.of(car)));
     }
 
-    @Ignore
     @Test
-    public void thenOneTrackWhenTwoParkingCars() {
-        ParkingCars parking = new ParkingCars(2, 2);
+    public void thenOneTrackWhenOneParkingCars() {
+        ParkingCars parking = new ParkingCars(0, 1);
         Car track = new Car("Track1", 2);
         parking.add(track);
-        List<Car> expected = new ArrayList<>(4);
-        expected.add(track);
-        expected.add(track);
-        assertThat(parking.findAll(), is(expected));
+        assertThat(parking.findAll(), is(List.of(track)));
     }
 
-    @Ignore
     @Test
-    public void thenOneCarAndOneTkackWhenThreeParkingCars() {
-        ParkingCars parking = new ParkingCars(2, 2);
+    public void thenOneCarAndOneTrackWhenTwoParkingCars() {
+        ParkingCars parking = new ParkingCars(1, 1);
         Car car = new Car("Car", 1);
         Car track = new Car("Track1", 2);
         parking.add(car);
         parking.add(track);
-        List<Car> expected = new ArrayList<>(4);
-        expected.add(car);
-        expected.add(track);
-        expected.add(track);
-        assertThat(parking.findAll(), is(expected));
+        assertThat(parking.findAll(), is(List.of(car, track)));
     }
 
-    @Ignore
     @Test
     public void thenTwoCarsWhenTwoParkingCars() {
         ParkingCars parking = new ParkingCars(2, 2);
@@ -54,13 +39,9 @@ public class ParkingCarsTest {
         Car car2 = new Car("Car2", 1);
         parking.add(car1);
         parking.add(car2);
-        List<Car> expected = new ArrayList<>(4);
-        expected.add(car1);
-        expected.add(car2);
-        assertThat(parking.findAll(), is(expected));
+        assertThat(parking.findAll(), is(List.of(car1, car2)));
     }
 
-    @Ignore
     @Test
     public void thenTwoTracksWhenTwoParkingCars() {
         ParkingCars parking = new ParkingCars(2, 2);
@@ -68,15 +49,9 @@ public class ParkingCarsTest {
         Car track2 = new Car("Track2", 2);
         parking.add(track1);
         parking.add(track2);
-        List<Car> expected = new ArrayList<>(4);
-        expected.add(track1);
-        expected.add(track1);
-        expected.add(track2);
-        expected.add(track2);
-        assertThat(parking.findAll(), is(expected));
+        assertThat(parking.findAll(), is(List.of(track1, track2)));
     }
 
-    @Ignore
     @Test(expected = IllegalArgumentException.class)
     public void thenThreeCarsWhenNoPlaces() {
         ParkingCars parking = new ParkingCars(2, 2);
@@ -88,7 +63,6 @@ public class ParkingCarsTest {
         parking.add(car3);
     }
 
-    @Ignore
     @Test
     public void thenThreeTracksWhenFourParkingCars() {
         ParkingCars parking = new ParkingCars(2, 2);
@@ -98,15 +72,9 @@ public class ParkingCarsTest {
         parking.add(track1);
         parking.add(track2);
         parking.add(track3);
-        List<Car> expected = new ArrayList<>(4);
-        expected.add(track1);
-        expected.add(track2);
-        expected.add(track3);
-        expected.add(track3);
-        assertThat(parking.findAll(), is(expected));
+        assertThat(parking.findAll(), is(List.of(track1, track2, track3, track3)));
     }
 
-    @Ignore
     @Test(expected = IllegalArgumentException.class)
     public void thenOneCarAndThreeTracksWhenNoPlaces() {
         ParkingCars parking = new ParkingCars(2, 2);
@@ -120,7 +88,6 @@ public class ParkingCarsTest {
         parking.add(track3);
     }
 
-    @Ignore
     @Test(expected = IllegalArgumentException.class)
     public void thenOneCarWhenNoPlaces() {
         ParkingCars parking = new ParkingCars(0, 2);
@@ -128,7 +95,6 @@ public class ParkingCarsTest {
         parking.add(car);
     }
 
-    @Ignore
     @Test(expected = IllegalArgumentException.class)
     public void thenOneTrackWhenNoPlaces() {
         ParkingCars parking = new ParkingCars(1, 0);
@@ -136,7 +102,6 @@ public class ParkingCarsTest {
         parking.add(track);
     }
 
-    @Ignore
     @Test
     public void thenTwoSuperTracksWhenFourParkingCars() {
         ParkingCars parking = new ParkingCars(3, 1);
@@ -144,15 +109,10 @@ public class ParkingCarsTest {
         Car superTrack2 = new Car("SuperTrack2", 3);
         parking.add(superTrack1);
         parking.add(superTrack2);
-        List<Car> expected = new ArrayList<>(4);
-        expected.add(superTrack1);
-        expected.add(superTrack2);
-        expected.add(superTrack2);
-        expected.add(superTrack2);
-        assertThat(parking.findAll(), is(expected));
+        assertThat(parking.findAll(), is(List.of(
+                superTrack1, superTrack2, superTrack2, superTrack2)));
     }
 
-    @Ignore
     @Test(expected = IllegalArgumentException.class)
     public void thenOneCarAndTwoSuperTracksWhenNoPlaces() {
         ParkingCars parking = new ParkingCars(3, 1);
